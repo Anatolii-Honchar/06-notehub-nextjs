23b7585c32ae3c26a613354aa7a2f5979ef70429
Anatolii Honchar <gonchar_tol@ukr.net>
2026-03-27 14:23:01 +0200

    add: components, Header, Footer

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>© 2024 NoteHub. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ components/Header/Header.tsx
@@ -0,0 +1,21 @@
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/notes">Notes</a>
+          </li>
+          <li>
+            <a href="/profile">Profile</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
